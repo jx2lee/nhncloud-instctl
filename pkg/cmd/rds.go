af1b432e048e732c706721ebfa607ce177f6e59d
jx2lee <jaejun.lee.1991@gmail.com>
2021-04-03 20:32:32 +0900

    add: rds module
    * rds endpoint 모듈 추가

---
+++ pkg/cmd/rds.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"github.com/jx2lee/nhncloud-instctl/pkg/rds"
+
+	"github.com/spf13/cobra"
+)
+
+var rdsCmd = &cobra.Command{
+	Use:   "rds",
+	Short: "NHN Cloud RDS-Related Commands",
+}
+
+var getPortInfoCmd = &cobra.Command{
+	Use:   "get-port",
+	Short: "Look up the port on the iaas instance.",
+	Run: func(cmd *cobra.Command, args []string) {
+		rds.GetIaasInstancePortInfo(IaasInstanceId)
+	},
+}
+
+var attachFipCmd = &cobra.Command{
+	Use:   "attach-fip",
+	Short: "Connect the fip to the new instance.",
+	Run: func(cmd *cobra.Command, args []string) {
+		rds.AttachFip(FIPId, PortId)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(rdsCmd)
+	rdsCmd.AddCommand(getPortInfoCmd)
+	rdsCmd.AddCommand(attachFipCmd)
+
+	getPortInfoCmd.Flags().StringVarP(&IaasInstanceId, "iaas-id", "i", "", "set the iaas instance ID that connected the FIP.")
+	getPortInfoCmd.MarkFlagRequired("iaas-id")
+
+	attachFipCmd.Flags().StringVarP(&FIPId, "fip-id", "f", "", "set the floating_ip_id.")
+	attachFipCmd.Flags().StringVarP(&PortId, "port-id", "p", "", "set the port_id.")
+	attachFipCmd.MarkFlagRequired("fip-id")
+	attachFipCmd.MarkFlagRequired("port-id")
+
+}
---
+++ pkg/rds/rds.go
@@ -0,0 +1,142 @@
+package rds
+
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/jx2lee/nhncloud-instctl/pkg/instance"
+)
+
+type ToastConfig = instance.ToastConfig
+type PayloadAttachFIP struct {
+	Floatingip Floatingip `json:"floatingip"`
+}
+type Floatingip struct {
+	PortID string `json:"port_id"`
+}
+
+
+func requestRDSToken() (*ToastConfig, error) {
+	tenantID, userName, passWord, _ := instance.GetEnvparser("rds").GetPasswordCredentials()
+	tokenURL := "https://api-identity.infrastructure.cloud.toast.com/v2.0/tokens"
+
+	data := instance.Payload{
+		Auth: instance.Auth{
+			TenantID: tenantID,
+			PasswordCredentials: instance.PasswordCredentials{
+				Username: userName,
+				Password: passWord,
+			},
+		},
+	}
+
+	payloadBytes, err := json.Marshal(data)
+	if err != nil {
+		logrus.Fatal("Unable to encode data: ", data)
+	}
+
+	body := bytes.NewReader(payloadBytes)
+	req, err := http.NewRequest("POST", tokenURL, body)
+	if err != nil {
+		logrus.Fatal("Request Failed: " + tokenURL)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		logrus.Fatal("Unable to use client")
+	}
+
+	defer resp.Body.Close()
+
+	allRespBytes, _ := ioutil.ReadAll(resp.Body)
+	allRespMap := make(map[string]interface{})
+	if err := json.Unmarshal(allRespBytes, &allRespMap); err != nil {
+		logrus.Fatal(err)
+	}
+
+	tokenInfo := allRespMap["access"].(interface{}).(map[string]interface{})["token"]
+
+	toastConfig := instance.ToastConfig{}
+	toastConfig.TokenID = fmt.Sprint(tokenInfo.(interface{}).(map[string]interface{})["id"])
+	toastConfig.ExpireDate = fmt.Sprint(tokenInfo.(interface{}).(map[string]interface{})["expires"])
+	return &toastConfig, nil
+}
+
+func GetIaasInstancePortInfo(iaasInstanceId string) {
+	tokenInfo, err := requestRDSToken()
+	logrus.Info("Token-ID: ", tokenInfo.TokenID)
+	if err != nil {
+		logrus.Fatal("Failed Get Token.")
+	}
+
+	var baseURL = "http://iaas.tcc1.cloud.toastoven.net/network/v2.0/ports?device_id=" + iaasInstanceId
+	req, err := http.NewRequest("GET", baseURL, nil)
+	req.Header.Set("X-Auth-Token", tokenInfo.TokenID)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		logrus.Fatal("No region have been selected. Please select kr1, kr2, or jp1.")
+	}
+
+	defer resp.Body.Close()
+	allRespBytes, _ := ioutil.ReadAll(resp.Body)
+	allRespMap := make(map[string]interface{})
+	if err := json.Unmarshal(allRespBytes, &allRespMap); err != nil {
+		logrus.Fatal(err)
+	}
+
+	portInfo, _ := prettyPrintJSON(allRespBytes)
+	fmt.Printf("%s", portInfo)
+}
+
+func AttachFip(floatingIpId string, portId string) {
+	tokenInfo, err := requestRDSToken()
+	if err != nil {
+		logrus.Fatal("Failed Get Token.")
+	}
+
+	data := PayloadAttachFIP{
+		Floatingip{
+			PortID: portId,
+		},
+	}
+	payloadAttachFIPBytes, err := json.Marshal(data)
+	if err != nil {
+		logrus.Fatal("Hmm..")
+	}
+	body := bytes.NewReader(payloadAttachFIPBytes)
+
+	var baseURL = "http://iaas.tcc1.cloud.toastoven.net/network/v2.0/floatingips/" + floatingIpId
+	req, err := http.NewRequest("PUT", baseURL, body)
+	if err != nil {
+		logrus.Fatal("Hmm..")
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Auth-Token", tokenInfo.TokenID)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		logrus.Fatal("Hmm..")
+	}
+	defer resp.Body.Close()
+	allRespBytes, _ := ioutil.ReadAll(resp.Body)
+	allRespMap := make(map[string]interface{})
+	if err := json.Unmarshal(allRespBytes, &allRespMap); err != nil {
+		logrus.Fatal(err)
+	}
+
+	responseAttachInfo, _ := prettyPrintJSON(allRespBytes)
+	fmt.Printf("%s", responseAttachInfo)
+}
+
+func prettyPrintJSON(b []byte) ([]byte, error) {
+	var out bytes.Buffer
+	err := json.Indent(&out, b, "", "  ")
+	return out.Bytes(), err
+}
